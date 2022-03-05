24a3a9cd22429e89c7ba910d9959b7bc7fc83e75
Adam Bezecny <adam.bezecny@gmail.com>
2022-03-05 12:02:47 +0100

    some data types added

--- main.go
+++ main.go
@@ -4,9 +4,24 @@ import (
 	"context"
 	"github.com/dapr/go-sdk/service/common"
 	daprd "github.com/dapr/go-sdk/service/http"
+	"github.com/m-to-n/channels-backend-services/data"
 	"log"
 	"net/http"
 )
+
+func foo() data.ChannelType {
+	return data.Unknown
+}
+
+func cronHandler(ctx context.Context, in *common.BindingEvent) (out []byte, err error) {
+	log.Printf("cronHandler binding - Data:%s, Meta:%v", in.Data, in.Metadata)
+	return nil, nil
+}
+
+func sqsHandler(ctx context.Context, in *common.BindingEvent) (out []byte, err error) {
+	log.Printf("sqsHandler binding - Data:%s, Meta:%v", in.Data, in.Metadata)
+	return nil, nil
+}
 
 func main() {
 	s := daprd.NewService(":6002")
@@ -23,14 +38,4 @@ func main() {
 	if err := s.Start(); err != nil && err != http.ErrServerClosed {
 		log.Fatalf("error listenning: %v", err)
 	}
-}
-
-func cronHandler(ctx context.Context, in *common.BindingEvent) (out []byte, err error) {
-	log.Printf("cronHandler binding - Data:%s, Meta:%v", in.Data, in.Metadata)
-	return nil, nil
-}
-
-func sqsHandler(ctx context.Context, in *common.BindingEvent) (out []byte, err error) {
-	log.Printf("sqsHandler binding - Data:%s, Meta:%v", in.Data, in.Metadata)
-	return nil, nil
 }
