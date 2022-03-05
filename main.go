178d4e5b21827a10580829078325e84262fdf299
Adam Bezecny <adam.bezecny@gmail.com>
2022-03-05 18:13:27 +0100

    tw payload parsing

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/dapr/go-sdk/service/common"
 	daprd "github.com/dapr/go-sdk/service/http"
 	"github.com/m-to-n/common/channels"
+	whatsapp "github.com/m-to-n/common/channels/whatsapp-twilio"
 	"log"
 	"net/http"
 )
@@ -20,6 +23,18 @@ func cronHandler(ctx context.Context, in *common.BindingEvent) (out []byte, err 
 
 func sqsHandler(ctx context.Context, in *common.BindingEvent) (out []byte, err error) {
 	log.Printf("sqsHandler binding - Data:%s, Meta:%v", in.Data, in.Metadata)
+
+	var tReq whatsapp.TwilioRequest
+
+	errParse := json.Unmarshal(in.Data, &tReq)
+	if err != nil {
+		log.Printf("sqsHandler: error when unamrshaling sqs payload: #{errParse}")
+		return nil, errParse
+	}
+
+	// move struct pretty print to common
+	fmt.Printf("twilio request struct: %#v\n", tReq)
+
 	return nil, nil
 }
 
